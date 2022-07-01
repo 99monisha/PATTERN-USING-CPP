c7833d6c45c0b8066beb7a1f76ffcea8f6f501ca
Monisha Dolai <monishadolai1998@gmail.com>
2022-07-01 13:50:12 +0530

    pattern 9

---
+++ DAY-1/pattern-9.cpp
@@ -0,0 +1,29 @@
+/*
+    1
+   22
+  333
+ 4444
+55555
+
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j,k;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(k=1;k<=n-i;k++)
+        {
+            cout<<" ";
+        }
+        for(j=1;j<=i;j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
