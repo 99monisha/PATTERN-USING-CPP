a43b4f346c78ea394569d2af7dfe5b7825a496bd
Monisha Dolai <monishadolai1998@gmail.com>
2022-07-04 12:05:16 +0530

    pattern-19

---
+++ DAY-1/pattern-19.cpp
@@ -0,0 +1,31 @@
+/*
+5
+    1 
+   1 2 
+  1 2 3 
+ 1 2 3 4 
+1 2 3 4 5 
+
+*/
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j,k;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(k=1;k<=n-i;k++)
+        {
+            cout<<" ";
+        }
+        for(j=1;j<=i;j++)
+        {
+            cout<<"j"<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
